8326775f0b76d9b6e8b3b8e46ba4b51f8982be9f
Devsanthosh <trademarkia@Trademarkias-MacBook-Air.local>
2024-10-28 20:49:50 +0530

    aws-auth

--- df-vars.tf
+++ df-vars.tf
@@ -7,21 +7,3 @@ resource "kubernetes_config_map" "aws_auth" {
     name      = "aws-auth"
     namespace = "kube-system"
   }
-
-  data = {
-    mapRoles = <<EOT
-- groups:
-  - system:bootstrappers
-  - system:nodes
-  rolearn: arn:aws:iam::825765392239:role/my-demo-tf-cluster-eks-node-role
-  username: system:node:{{EC2PrivateDNSName}}
-EOT
-
-    mapUsers = <<EOT
-- groups:
-  - system:masters
-  userarn: arn:aws:iam::825765392239:user/devsanthosh
-  username: devsanthosh
-EOT
-  }
-}
